e5682eeef6cf1c0cc74efea5032be7b5f466e5cd
sxj170022 <47926577+sxj170022@users.noreply.github.com>
2019-02-23 18:55:16 -0600

    Add files via upload

---
+++ LogInBank2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author surya
+ */
+public class LogInBank2 {
+
+    // class for printing out asteriks
+    private static void asteriks(int count){
+            for(int i=0;i<count;++i)
+                System.out.print("*");
+    }
+    // class for printing space
+    private static void spaces (int count)
+    {
+        for(int i=0;i<count;++i)
+            System.out.print(" ");
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
